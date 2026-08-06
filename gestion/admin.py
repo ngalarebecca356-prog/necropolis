23eebb257ae96a96a00b768d094bb494c9745691
Rebecca <ngalarebecca356@gmail.com>
2026-08-06 20:49:23 +0100

    Correction finale et exacte de admin.py (plus de fautes de frappe)

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,72 +1,53 @@
 from django.contrib import admin
-from .models import Caveau, Defunt, Reservation, Concession, Exhumation, Profil, AuditLog, Cimetiere
-from datetime import date
+from .models import (
+    Cimetiere, Caveau, Defunt, Reservation, Concession, 
+    Exhumation, Profil, Transaction, CodeInvitation, AuditLog, SessionToken
+)
 
 @admin.register(Cimetiere)
 class CimetiereAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'ville', 'pays', 'superficie_totale', 'nombre_places_estime', 'admin_createur', 'date_creation')
-    search_fields = ('nom', 'ville', 'pays')
-    readonly_fields = ('mot_de_passe_acces',)
+    list_display = ('nom', 'ville', 'pays', 'superficie_totale')
 
 @admin.register(Caveau)
 class CaveauAdmin(admin.ModelAdmin):
-    list_display = ('numero', 'section', 'bloc', 'type_cave')
-    list_filter = ('statut', 'section', 'bloc')
-    search_fields = ('numero', 'localisation')
+    # SEULEMENT les champs qui existent vraiment dans models.py
+    list_display = ('numero', 'section', 'bloc', 'type_caveau', 'statut', 'cimetiere')
+    list_filter = ('statut', 'type_caveau', 'cimetiere')
+    search_fields = ('numero', 'section', 'bloc')
 
 @admin.register(Defunt)
 class DefuntAdmin(admin.ModelAdmin):
     list_display = ('nom', 'prenom', 'date_deces', 'caveau')
-    search_fields = ('nom', 'prenom')
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ('client', 'caveau', 'defunt', 'statut', 'date_reservation')
+    list_display = ('id', 'caveau', 'client', 'statut', 'date_reservation')
     list_filter = ('statut',)
-    search_fields = ('client',)
 
 @admin.register(Concession)
 class ConcessionAdmin(admin.ModelAdmin):
-    list_display = ('caveau', 'beneficiaire', 'client', 'type_concession', 'date_debut', 'date_fin', 'montant', 'jours_restants')
-    list_filter = ('type_concession', 'renouvele')
-    search_fields = ('beneficiaire', 'client', 'caveau__numero')
-    date_hierarchy = 'date_fin'
-
-    def jours_restants(self, obj):
-        if obj.date_fin:
-            delta = (obj.date_fin - date.today()).days
-            if delta < 0:
-                return "Expiré"
-            return f"{delta} j"
-        return "-"
-    jours_restants.short_description = "Reste"
+    list_display = ('id', 'caveau', 'client', 'type_concession', 'date_debut')
 
 @admin.register(Exhumation)
 class ExhumationAdmin(admin.ModelAdmin):
-    list_display = ('defunt', 'caveau', 'date_demande', 'statut')
-    list_filter = ('statut',)
-
-from .models import Profil, AuditLog
+    list_display = ('id', 'caveau', 'defunt', 'statut', 'date_demande')
 
 @admin.register(Profil)
 class ProfilAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role', 'statut', 'date_creation')
-    list_filter = ('role', 'statut')
-    search_fields = ('user__username', 'user__email')
-    actions = ['valider_comptes', 'refuser_comptes']
+    list_display = ('user', 'role', 'statut', 'cimetiere')
 
-    def valider_comptes(self, request, queryset):
-        queryset.update(statut='valide')
-    valider_comptes.short_description = "Valider les comptes selectionnes"
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'reservation', 'montant', 'methode', 'date_transaction')
 
-    def refuser_comptes(self, request, queryset):
-        queryset.update(statut='refuse')
-    refuser_comptes.short_description = "Refuser les comptes selectionnes"
-
+@admin.register(CodeInvitation)
+class CodeInvitationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'cimetiere', 'role_autorise', 'utilise')
 
 @admin.register(AuditLog)
 class AuditLogAdmin(admin.ModelAdmin):
-    list_display = ('date', 'utilisateur', 'action', 'objet_type', 'objet_id')
-    list_filter = ('action', 'objet_type')
-    search_fields = ('utilisateur__username', 'action')
-    date_hierarchy = 'date'
+    list_display = ('date', 'utilisateur', 'action', 'objet_type')
+
+@admin.register(SessionToken)
+class SessionTokenAdmin(admin.ModelAdmin):
+    list_display = ('utilisateur', 'cle', 'date_creation')
